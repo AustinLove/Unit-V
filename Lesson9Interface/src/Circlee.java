560d2b0ad0ece898bf50bed2a52a6c9c9fb2dfc8
Austin Love <Austin Love@172.17.24.28>
2014-11-27 08:11:51 -0500

    Une Circlee and Recte not circle and rect

--- Lesson9Interface/src/Circlee.java
+++ Lesson9Interface/src/Circlee.java
@@ -2,24 +2,24 @@
 import TurtleGraphics.Pen;
 
 
-public class Circlee implements Shape{
-    private double xPos, yPos, radius;
+public class Circlee extends AbstractShape{
+    protected double radius;
     
     public Circlee(){
-        xPos = 0;
-        yPos = 0;
+        super();
         radius = 1;
     }
-    public Circlee(double xLoc, double yLoc, double r){
-        xPos = xLoc;
-        yPos = yLoc;
+    public Circlee(double x, double y, double r){
+        super(x,y);
         radius = r;
     }
     public double area(){
         return Math.PI *radius*radius;
     }
-
-    @Override
+    public void stretchBy(double factor){
+        radius*=factor;
+    }
+    
     public void draw(Pen p) {
         double side = 2.0*Math.PI *radius/120.0;
         p.up();
@@ -32,33 +32,15 @@ public class Circlee implements Shape{
             p.turn(3);
         }
     }
-
-    @Override
-    public double getXPos() {
-        return xPos;
-    }
-
-    @Override
-    public double getYPos() {
-        return yPos;
-    }
-
-    @Override
-    public void move(double xLoc, double yLoc) {
-        xPos = xLoc;
-        yPos = yLoc;
-    }
-
-    @Override
-    public void stretchBy(double factor) {
-        radius *= factor;
-    }
     public String toString(){
-        String str = "CIRCLE\n"
-                + "Radius: " +radius+"\n"
-                +"(X,Y) Position: (" +xPos+"," +yPos+")\n"
-                +"Area: " +area();
+        String str = "This is a Circlee\n";
+        str+="Radius = " +radius;
+        str+="\n" +super.toString();
         return str;
     }
-    
+    public double per(){
+        double per;
+       per= 2.0* Math.PI* radius;
+       return per;
+    }
 }
